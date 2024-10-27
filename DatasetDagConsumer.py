3954d82f5f9b539355ca671d522942112b8c7764
abhitarkase <abhijeettarkase143@gmail.com>
2024-10-27 17:19:53 +0530

    master

--- DatasetDagConsumer.py
+++ DatasetDagConsumer.py
@@ -14,8 +14,9 @@ dag=DAG(
     catchup=False,
     start_date=DateTime(2022,12,30)
 )
-
 #outlets in config tells interpreter that this method/task returns/update a dataset 
+#don't use outlets config in a task that triggers after the dataset in modified 
+#if we do that then the task will never ends.it will trigger in a while loop
 @task(task_id="Consumer",dag=dag)
 def consumer(**kwargs):
     print("abc file is reading")
